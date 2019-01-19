ef9edcb19f73abd2492f3d38df5d1a591a8779dc
dmrfcoder <dmrfcoder@dmrfcoderdeMacBook-Pro.local>
2019-01-19 10:32:49 +0800

    keras tutorial happy-house

--- KerasTutorialHappyHouse/happy_house.py
+++ KerasTutorialHappyHouse/happy_house.py
@@ -4,6 +4,8 @@ from keras.layers import Input, Dense, Activation, ZeroPadding2D, BatchNormaliza
 from keras.layers import AveragePooling2D, MaxPooling2D, Dropout, GlobalMaxPooling2D, GlobalAveragePooling2D
 from keras.models import Model
 from keras.preprocessing import image
+from PIL import Image
+
 from keras.utils import layer_utils
 from keras.utils.data_utils import get_file
 from keras.applications.imagenet_utils import preprocess_input
@@ -20,8 +22,6 @@ import matplotlib.pyplot as plt
 from matplotlib.pyplot import imshow
 
 
-# GRADED FUNCTION: HappyModel
-
 def HappyModel(input_shape):
     """
     Implementation of the HappyModel.
@@ -33,7 +33,6 @@ def HappyModel(input_shape):
     model -- a Model() instance in Keras
     """
 
-    ### START CODE HERE ###
     # Feel free to use the suggested outline in the text above to get started, and run through the whole
     # exercise (including the later portions of this notebook) once. The come back also try out other
     # network architectures as well.
@@ -57,12 +56,10 @@ def HappyModel(input_shape):
     # Create model. This creates your Keras model instance, you'll use this instance to train/test the model.
     model = Model(inputs=X_input, outputs=X, name='HappyModel')
 
-    ### END CODE HERE ###
-
-
     return model
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     X_train_orig, Y_train_orig, X_test_orig, Y_test_orig, classes = load_dataset()
 
     # Normalize image vectors
@@ -79,26 +76,33 @@ if __name__=='__main__':
     print("Y_train shape: " + str(Y_train.shape))
     print("X_test shape: " + str(X_test.shape))
     print("Y_test shape: " + str(Y_test.shape))
+
+    # create the model.
     happyModel = HappyModel(X_train[0].shape)
+    #
+    '''
+    loss:use binary_crossentropy loss function for the binary classification 
+    optimizer:use adam optimizer
+    '''
     happyModel.compile(loss='binary_crossentropy', optimizer="adam", metrics=['accuracy'])
-    happyModel.fit(X_train,Y_train,batch_size=16,epochs=8)
+    # Choose the number of epochs and the batch size
+    happyModel.fit(X_train, Y_train, batch_size=16, epochs=8)
+
     preds = happyModel.evaluate(X_test, Y_test, batch_size=32)
-    ### END CODE HERE ###
     print()
     print("Loss = " + str(preds[0]))
     print("Test Accuracy = " + str(preds[1]))
-    ### START CODE HERE ###
+
     img_path = 'my_image.jpg'
-    ### END CODE HERE ###
     img = image.load_img(img_path, target_size=(64, 64))
     imshow(img)
+    plt.show()
 
     x = image.img_to_array(img)
     x = np.expand_dims(x, axis=0)
     x = preprocess_input(x)
-
+    print('predict:')
     print(happyModel.predict(x))
     happyModel.summary()
     plot_model(happyModel, to_file='HappyModel.png')
-    #SVG(model_to_dot(happyModel).create(prog='dot', format='svg'))
-
+    SVG(model_to_dot(happyModel).create(prog='dot', format='svg'))
